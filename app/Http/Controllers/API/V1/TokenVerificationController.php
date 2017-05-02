d7281826c1d19119105813479cd8df509cdc0e7e
Alex Markessinis <markea@sage.edu>
2017-05-02 00:08:45 -0400

    converted to orm php client

--- app/Http/Controllers/API/V1/TokenVerificationController.php
+++ app/Http/Controllers/API/V1/TokenVerificationController.php
@@ -3,36 +3,33 @@
 namespace App\Http\Controllers\API\V1;
 
 use Illuminate\Http\Request;
-use Unirest\Request\Body;
-use Unirest\Request as Unirequest;
+use OpenResourceManager\Client\ResourceVerification as VerificationClient;
+
 
 class TokenVerificationController extends ApiController
 {
-
+    /**
+     * @param Request $request
+     * @param null $token
+     * @return \Dingo\Api\Http\Response|void
+     */
     public function verify(Request $request, $token = null)
     {
         if ($request->isMethod('post') && empty($token)) {
             $data = $request->all();
             $token = $data['token'];
         }
-        $ormUrl = fixPath(env('ORM_API_URL', ''));
-        if (!empty($ormUrl)) {
-            $jwt = $this->authenticateWithOrm();
-            $ormUrl = $ormUrl . 'verify/';
-            $body = Body::form(array('token' => $token));
-            $headers = array('Accept' => 'application/json', 'Authorization' => 'Bearer ' . $jwt);
-            $response = Unirequest::post($ormUrl, $headers, $body);
-            if ($response->code == 202) {
-                return $this->response->accepted('/', ['message' => $response->body->message, 'code' => $response->code]);
-            } elseif ($response->code == 404) {
-                return $this->response->accepted('/', ['message' => $response->body->message, 'code' => $response->code]);
-            } else {
-                return $this->response->errorInternal($response->body->message, $response->code);
-            }
+        $orm = getORMConnection();
+        $verifyClient = new VerificationClient($orm);
+        $response = $verifyClient->postVerification($token);
+        if ($response->code == 202) {
+            return $this->response->accepted('/', ['message' => $response->body->message, 'code' => $response->code]);
+        } elseif ($response->code == 404) {
+            return $this->response->accepted('/', ['message' => $response->body->message, 'code' => $response->code]);
         } else {
-            // Throw an error if we don't have the ORM URL or Key
-            return $this->response->errorInternal('Missing ORM URL!');
+            return $this->response->errorInternal($response->body->message, $response->code);
         }
+
     }
 
 }
--- app/helpers.php
+++ app/helpers.php
@@ -6,6 +6,21 @@
  * Time: 8:53 PM
  */
 
+use OpenResourceManager\ORM;
+
+/**
+ * Get an ORM connection
+ *
+ * Reads environment vars and returns an ORM connection.
+ *
+ * @return ORM
+ */
+function getORMConnection()
+{
+    $orm = new ORM(env('ORM_API_SECRET', ''), env('ORM_HOST', 'localhost'), env('ORM_VERSION', 1), env('ORM_PORT', 80), env('ORM_USE_HTTPS', false));
+    return $orm;
+}
+
 /**
  * @param string $path
  * @return mixed|string
